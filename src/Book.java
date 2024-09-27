9890b130cc4e4a3267e024c3a53380eb48e6b1c7
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-09-27 19:15:18 +0200

    Added Book

---
+++ src/Book.java
@@ -0,0 +1,53 @@
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+    private int pages;
+    private String publisher;
+
+    // Const
+    public Book(String title, String author, String isbn, boolean isAvailable, int pages, String publisher) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+
+    // TODO Get & Setter start
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    public boolean isAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+        return isAvailable;
+    }
+    // TODO Get & Setter end
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", isAvailable=" + isAvailable +
+                ", pages=" + pages +
+                ", publisher='" + publisher + '\'' +
+                '}';
+    }
+}
