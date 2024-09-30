fde4523d18a26827540a9034b831062121425a11
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-09-30 16:27:24 +0200

    librarySystem

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
-
-// 27 Sep 2024 Jonas N Library-v2
+// 28 sep 2024 Jonas Upp OOP Library-App-v2
 
 public class Main {
     public static void main(String[] args) {
+
+        // Skapa ett objekt av LibrarySystem och starta menyn
+        LibrarySystem librarySystem = new LibrarySystem();
+        librarySystem.menu();
 
     }
 }
