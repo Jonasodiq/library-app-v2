6111469ada92391bd376e8bd3c76909a23678e54
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-09-30 19:07:47 +0200

    Added link UML

--- src/Main.java
+++ src/Main.java
@@ -9,3 +9,7 @@ public class Main {
 
     }
 }
+
+/*
+  link to -> UML diagram https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1#Wb!IcJh-XBgx0m0NAPZjUQ8l_AtKQCw-k5Bh4fDWg3bUEOa2cM-Xuy6S691_ieoAW3L%2F016TN5E3VSPI62ZQU65BBLQZ7T4QS5N3Z3
+ */
