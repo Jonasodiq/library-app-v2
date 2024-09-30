ca5a481e388b36098343d3a5475f56a9d046fa33
Jonas Niyazson <jonas.niyazson@studerande.karlskogafhs.se>
2024-09-30 16:27:52 +0200

    Change

--- src/Book.java
+++ src/Book.java
@@ -1,55 +1,21 @@
-public class Book {
-    private String title;
-    private String author;
-    private String isbn;
-    private boolean isAvailable;
-    private int pages;
-    private String publisher;
 
-    // Const
-    public Book(String title, String author, String isbn) {
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.isAvailable = isAvailable;
-    }
+import java.util.Date;
 
-    // TODO Get & Setter start
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public String getAuthor() {
-        return author;
-    }
-    public void setAuthor(String author) {
+class Book extends Media {
+    private final String author;
+
+    public Book(String title, Date pubDate, String author) {
+        super(title, pubDate);
         this.author = author;
     }
-    public String getIsbn() {
-        return isbn;
-    }
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-    public void setAvailable(boolean isAvailable) {
-        this.isAvailable = isAvailable;
-    }
-    // TODO Get & Setter end
 
     @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", isAvailable=" + isAvailable +
-                ", pages=" + pages +
-                ", publisher='" + publisher + '\'' +
-                '}';
+    public String getCreator() {
+        return author;
+    }
+
+    @Override
+    public int getLoanPeriod() {
+        return 14;  // Vanlig låneperiod för böcker är 14 dagar
     }
 }
